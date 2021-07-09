d8878be571730db57407961e1fe7073226c61b52
Emanuele Coccia <78683034+3m4c@users.noreply.github.com>
2021-07-09 15:23:21 +0200

    Update sentiment_algo.py

--- WSB/Sentiment Analysis/sentiment_algo.py
+++ WSB/Sentiment Analysis/sentiment_algo.py
@@ -6,7 +6,31 @@ connection = psycopg2.connect(host = config.DB_HOST, database = config.DB_NAME, 
 cursor = connection.cursor(cursor_factory = psycopg2.extras.DictCursor)
 
 cursor.execute(
-    '''SELECT message, stock_symbol,  FROM mention'''
+    '''SELECT stock_id, stock_symbol, message, score, num_comments FROM mention'''
 )
 rows = cursor.fetchall()
+
+def WSB_sentiment (message, score, num_comments):
+    
+    bullish = ['moon', 'mars', 'yolo', 'all', 'strong', "can't", 'tits', 'c', 'calls', 'call', 'btfd', 'undervalued', 'gains', 'bull', 'bulls', 'bullish', 
+               'buy', 'dip', 'fuel', 'fire', 'squeeze', 'squeezing', 'squoze', 'squozing', 'holding',
+               '💎', '🤲', '💎🤲', '🤤🤤🤤', '🌝', '💎🤲🚀', '💎🙌🏻', '🙌🏻', '💎💎🙌🙌', '🚀💎👐', '💎✋🚀', 
+               '🦍', '🦍🦍', '🦍🦍🦍', '🦍🦍🦍🦍', '🦍🦍🦍🦍🦍', '🦍🦍🦍🦍🦍🦍', '🦍🦍🦍🦍🦍🦍🦍', '🦍🦍🦍🦍🦍🦍🦍🦍',
+               '🚀', '🚀🚀', 🚀🚀🚀', '🚀🚀🚀🚀', '🚀🚀🚀🚀🚀', '🚀🚀🚀🚀🚀🚀', '🚀🚀🚀🚀🚀🚀🚀', '🚀🚀🚀🚀🚀🚀🚀🚀', 
+               '🚀🚀🚀🚀🚀🚀🚀🚀🚀', '🚀🚀🚀🚀🚀🚀🚀🚀🚀🚀', '🌙', '🌙🌙', '🌙🌙🌙', '🚀📈', '📈'
+              ]
+    bearish = ['p', 'puts', 'put', 'losses', 'lose', 'bear', 'bears', 'bearish', 'gay',  '🌈🐻', '🌈', '🐻',
+               '😬', '😬😬',]
+    
+    bullish_count =
+    bearish_count = 
+    
+    words = message.lower().split()
+    for word in words:
+        if word in bullish:
+            
+        elif word in bearish:
+            
+        
+
 
