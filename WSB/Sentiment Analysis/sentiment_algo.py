c2d62729650d0405d1aa923605a3d80d957e15e3
Emanuele Coccia <78683034+3m4c@users.noreply.github.com>
2021-07-09 15:41:19 +0200

    Update sentiment_algo.py

--- WSB/Sentiment Analysis/sentiment_algo.py
+++ WSB/Sentiment Analysis/sentiment_algo.py
@@ -12,24 +12,29 @@ rows = cursor.fetchall()
 
 def WSB_sentiment (message, score, num_comments):
     
-    bullish = ['moon', 'mars', 'yolo', 'all', 'strong', "can't", 'tits', 'c', 'calls', 'call', 'btfd', 'undervalued', 'gains', 'bull', 'bulls', 'bullish', 
-               'buy', 'dip', 'fuel', 'fire', 'squeeze', 'squeezing', 'squoze', 'squozing', 'holding',
+    bullish = ['moon', 'mars', 'yolo', 'all', 'strong', "can't", 'tits', 'c', 'calls', 'call', 'btfd', 'undervalued', 'gains', 'gain', 'bull', 'bulls', 'bullish', 
+               'buy', 'dip', 'fuel', 'fire', 'squeeze', 'squeezing', 'squoze', 'squozing', 'holding', 'bought', 'hold', 'hodl', 'hodling', 'holding',
                '💎', '🤲', '💎🤲', '🤤🤤🤤', '🌝', '💎🤲🚀', '💎🙌🏻', '🙌🏻', '💎💎🙌🙌', '🚀💎👐', '💎✋🚀', 
                '🦍', '🦍🦍', '🦍🦍🦍', '🦍🦍🦍🦍', '🦍🦍🦍🦍🦍', '🦍🦍🦍🦍🦍🦍', '🦍🦍🦍🦍🦍🦍🦍', '🦍🦍🦍🦍🦍🦍🦍🦍',
                '🚀', '🚀🚀', 🚀🚀🚀', '🚀🚀🚀🚀', '🚀🚀🚀🚀🚀', '🚀🚀🚀🚀🚀🚀', '🚀🚀🚀🚀🚀🚀🚀', '🚀🚀🚀🚀🚀🚀🚀🚀', 
                '🚀🚀🚀🚀🚀🚀🚀🚀🚀', '🚀🚀🚀🚀🚀🚀🚀🚀🚀🚀', '🌙', '🌙🌙', '🌙🌙🌙', '🚀📈', '📈'
               ]
-    bearish = ['p', 'puts', 'put', 'losses', 'lose', 'bear', 'bears', 'bearish', 'gay',  '🌈🐻', '🌈', '🐻',
-               '😬', '😬😬',]
     
-    bullish_count =
-    bearish_count = 
+    bearish = ['p', 'puts', 'put', 'losses', 'lose', 'loss', 'bear', 'bears', 'bearish', 'gay', 'sold', 'sell', 'selling',
+               '🌈🐻', '🌈', '🐻', '😬', '😬😬'
+              ]
+    
+    bullish_count = 0
+    bearish_count = 0
     
     words = message.lower().split()
     for word in words:
         if word in bullish:
-            
+            bullish_count += 1
         elif word in bearish:
+            bearish_count += 1
+        return bullish_count
+        return bearish count
             
         
 
