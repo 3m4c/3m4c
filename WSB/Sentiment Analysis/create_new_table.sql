50fca719f1350b18a15405466485c2964ae1abf6
Emanuele Coccia <78683034+3m4c@users.noreply.github.com>
2021-07-10 20:35:24 +0200

    Update create_new_table.sql

--- WSB/Sentiment Analysis/create_new_table.sql
+++ WSB/Sentiment Analysis/create_new_table.sql
@@ -2,8 +2,9 @@ CREATE TABLE sentiment_evolution (
   stock_id INTEGER NOT NULL,
   dt TIMESTAMP WITHOUT TIME ZONE NOT NULL,
   stock_symbol TEXT NOT NULL,
-  stock_sentiment NUMERIC NOT NULL,
-  mention_id TEXT NOT NULL
+  mention_post_id TEXT NOT NULL,
+  bullish_sentiment NUMERIC NOT NULL DEFAULT 0,
+  bearish_sentiment NUMERIC NOT NULL DEFAULT 0
 );
 
 CREATE INDEX ON sentiment_evolution (stock_id, dt DESC);
