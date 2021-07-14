63d532f2ceb63e0488330fb190f49afc9738cf24
Emanuele Coccia <78683034+3m4c@users.noreply.github.com>
2021-07-14 15:41:59 +0200

    Update plots.py

--- WSB/Sentiment Analysis/plots.py
+++ WSB/Sentiment Analysis/plots.py
@@ -19,9 +19,17 @@ df = pd.DataFrame(data = rows, columns=('stock_symbol', 'dt', 'bullish_sentiment
 sorted_values = df.sort_values('stock_symbol')
 
 stock_to_plot = sorted_values[sorted_values['stock_symbol'] == 'AMC']
+del stock_to_plot['stock_symbol']
+
+#create new column to hold exponentially weighted moving average
+stock_to_plot['1dayEWM'] = stock_to_plot['bullish_sentiment'].ewm(span = 50, adjust=False).mean()
+print(stock_to_plot.head(20))
+stock_to_plot = stock_to_plot.melt('dt', var_name='cols',  value_name='vals')
+
+
 sns.relplot(
     data=stock_to_plot, kind="line",
-    x="dt", y="bullish_sentiment"
+    x = 'dt', y='vals', hue = 'cols'
 )
 
 plt.show()
