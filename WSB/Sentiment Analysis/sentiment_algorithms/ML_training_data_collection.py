d9cf84cb097252bbaf1cf3c88265be6335af7b6a
Emanuele Coccia <emanuele.coccia@studbocconi.it>
2021-07-22 22:55:21 +0200

    Update ML_training_data_collection.py

--- WSB/Sentiment Analysis/sentiment_algorithms/ML_training_data_collection.py
+++ WSB/Sentiment Analysis/sentiment_algorithms/ML_training_data_collection.py
@@ -6,12 +6,12 @@ connection = psycopg2.connect(host = config.DB_HOST, database = config.DB_NAME, 
 cursor = connection.cursor(cursor_factory = psycopg2.extras.DictCursor)
 
 cursor.execute("""
-    SELECT stock_symbol, post_id, title, body FROM mention
+    SELECT stock_symbol, post_id, title, body, flair FROM mention
 """)
 rows = cursor.fetchall()
 
 for row in rows:
-    print(f'{row["title"]} \n {row["body"]}')
+    print(f'{row["title"]} \n {row["flair"]} \n {row["body"]}')
     sentiment = 0
     sentiment_input = input(f'questo post ti sembra bullish (a), bearish (s) o neutral (d)?: ')
     if sentiment_input == 'a':
