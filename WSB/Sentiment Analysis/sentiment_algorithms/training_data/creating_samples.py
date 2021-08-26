b50e983c11517436e4827cbd28bcec67724cf559
Emanuele Coccia <emanuele.coccia@studbocconi.it>
2021-08-26 22:33:29 +0200

    Update creating_samples.py

--- WSB/Sentiment Analysis/sentiment_algorithms/training_data/creating_samples.py
+++ WSB/Sentiment Analysis/sentiment_algorithms/training_data/creating_samples.py
@@ -2,46 +2,39 @@ import config
 import psycopg2
 import psycopg2.extras
 import random
+import pandas as pd
 
 connection = psycopg2.connect(host=config.DB_HOST, database=config.DB_NAME, user=config.DB_USER,
                               password=config.DB_PASS)
 cursor = connection.cursor(cursor_factory=psycopg2.extras.DictCursor)
 
 # we take a third of samples from march 2020 crash (to train enough bearish posts), 
-# the we change the interval to fetch posts from january 2021
-# and then again from june 2021
+# a third from january 2021
+# and a third from june 2021
 
 cursor.execute("""
     SELECT stock_symbol, post_id, dt, title, body, flair, upvote_ratio 
     FROM mention
     WHERE dt::date BETWEEN '2020-02-13' AND '2020-03-27'
 """)
-rows = cursor.fetchall()
+sample1 = cursor.fetchall()
+random_sample1 = random.sample(rows, 500)
+random_sample1 = pd.DataFrame.to_csv()
 
-random_sample = random.sample(rows, 500) 
+cursor.execute("""
+    SELECT stock_symbol, post_id, dt, title, body, flair, upvote_ratio 
+    FROM mention
+    WHERE dt::date BETWEEN '2020-02-13' AND '2020-03-27'
+""")
+sample2 = cursor.fetchall()
+random_sample2 = random.sample(rows, 500)
+random_sample2 = pd.DataFrame.to_csv()
 
-for row in random_sample:
-    print(f'{row["title"]} \n {row["flair"]} \n {row["body"]}')
-    sentiment = 'neutral'
-    sentiment_input = input(f'questo post ti sembra bullish (a), bearish (s), neutral (d) o controversial (f)?: ')
-    if sentiment_input == 'a':
-        sentiment = 'bullish'
-    if sentiment_input == 's':
-        sentiment = 'bearish'
-    if sentiment_input == 'f':
-        sentiment = 'controversial'
-
-    post_id = row['post_id']
-    stock_symbol = row['stock_symbol']
-
-    try:
-        cursor.execute('''
-        UPDATE mention
-        SET sentiment = %s
-        WHERE post_id = %s AND stock_symbol = %s
-        ''', (sentiment, post_id, stock_symbol))
-        connection.commit()
-
-    except Exception as e:
-        print(e)
-        connection.rollback()
+cursor.execute("""
+    SELECT stock_symbol, post_id, dt, title, body, flair, upvote_ratio 
+    FROM mention
+    WHERE dt::date BETWEEN '2020-02-13' AND '2020-03-27'
+""")
+sample3 = cursor.fetchall()
+random_sample3 = random.sample(rows, 500)
+random_sample3 = pd.DataFrame.to_csv()
