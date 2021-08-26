2f6be649ef3466b06c3614fb105ef6e58f61faaa
Emanuele Coccia <emanuele.coccia@studbocconi.it>
2021-08-26 22:13:45 +0200

    Update ML_training_data_collection.py

--- WSB/Sentiment Analysis/sentiment_algorithms/ML_training_data_collection.py
+++ WSB/Sentiment Analysis/sentiment_algorithms/ML_training_data_collection.py
@@ -1,26 +1,35 @@
 import config
 import psycopg2
 import psycopg2.extras
+import random
 
 connection = psycopg2.connect(host=config.DB_HOST, database=config.DB_NAME, user=config.DB_USER,
                               password=config.DB_PASS)
 cursor = connection.cursor(cursor_factory=psycopg2.extras.DictCursor)
 
+# we take a third of samples from march 2020 crash (to train enough bearish posts), 
+# the we change the interval to fetch posts from january 2021
+# and then again from june 2021
+
 cursor.execute("""
-    SELECT stock_symbol, post_id, dt, title, body, flair 
+    SELECT stock_symbol, post_id, dt, title, body, flair, upvote_ratio 
     FROM mention
-    WHERE dt::date = '2021-06-09' --we then take some random days and label posts sentiment
+    WHERE dt::date BETWEEN '2020-02-13' AND '2020-03-27'
 """)
 rows = cursor.fetchall()
 
-for row in rows:
+random_sample = random.sample(rows, 500) 
+
+for row in random_sample:
     print(f'{row["title"]} \n {row["flair"]} \n {row["body"]}')
-    sentiment = 0
-    sentiment_input = input(f'questo post ti sembra bullish (a), bearish (s) o neutral (d)?: ')
+    sentiment = 'neutral'
+    sentiment_input = input(f'questo post ti sembra bullish (a), bearish (s), neutral (d) o controversial (f)?: ')
     if sentiment_input == 'a':
-        sentiment = 1
+        sentiment = 'bullish'
     if sentiment_input == 's':
-        sentiment = -1
+        sentiment = 'bearish'
+    if sentiment_input == 'f':
+        sentiment = 'controversial'
 
     post_id = row['post_id']
     stock_symbol = row['stock_symbol']
