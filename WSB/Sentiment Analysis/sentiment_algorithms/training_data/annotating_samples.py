10142985b7c043ff358ba6f07e04831771daabcf
Emanuele Coccia <emanuele.coccia@studbocconi.it>
2021-08-26 22:25:39 +0200

    Update annotating_samples.py

--- WSB/Sentiment Analysis/sentiment_algorithms/training_data/annotating_samples.py
+++ WSB/Sentiment Analysis/sentiment_algorithms/training_data/annotating_samples.py
@@ -1,4 +1,41 @@
 import config
 import psycopg2
 import psycopg2.extras
+import pandas as pd
 
+connection = psycopg2.connect(host=config.DB_HOST, database=config.DB_NAME, user=config.DB_USER,
+                              password=config.DB_PASS)
+cursor = connection.cursor(cursor_factory=psycopg2.extras.DictCursor)
+
+sample1 = pd.read_csv()
+sample2 = pd.read_csv()
+sample3 = pd.read_csv()
+
+
+for row in sample1:
+    print(f'{row["title"]} \n {row["flair"]} \n {row["body"]}')
+    sentiment = 'neutral'
+    sentiment_input = input(f'questo post ti sembra bullish (a), bearish (s), neutral (d) o controversial (f)?: ')
+    if sentiment_input == 'a':
+        sentiment = 'bullish'
+    if sentiment_input == 's':
+        sentiment = 'bearish'
+    if sentiment_input == 'd':
+        sentiment = 'neutral'
+    if sentiment_input == 'f':
+        sentiment = 'controversial'
+
+    post_id = row['post_id']
+    stock_symbol = row['stock_symbol']
+
+    try:
+        cursor.execute('''
+        UPDATE mention
+        SET sentiment = %s
+        WHERE post_id = %s AND stock_symbol = %s
+        ''', (sentiment, post_id, stock_symbol))
+        connection.commit()
+
+    except Exception as e:
+        print(e)
+        connection.rollback()
