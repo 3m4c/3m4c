99aa77323066ef2af72dcac5d9a323204e78b1d8
Emanuele Coccia <emanuele.coccia@studbocconi.it>
2021-09-07 22:35:55 +0200

    Update creating_samples.py

--- WSB/Sentiment Analysis/sentiment_algorithms/training_data/creating_samples.py
+++ WSB/Sentiment Analysis/sentiment_algorithms/training_data/creating_samples.py
@@ -19,7 +19,7 @@ cursor.execute("""
 """)
 sample1 = cursor.fetchall()
 random_sample1 = random.sample(sample1, 500)
-random_sample1 = pd.DataFrame.to_csv()
+random_sample1 = pd.DataFrame.to_csv('sample1.csv')
 
 cursor.execute("""
     SELECT stock_symbol, post_id, dt, title, body, flair, upvote_ratio 
@@ -28,7 +28,7 @@ cursor.execute("""
 """)
 sample2 = cursor.fetchall()
 random_sample2 = random.sample(sample2, 500)
-random_sample2 = pd.DataFrame.to_csv()
+random_sample2 = pd.DataFrame.to_csv('sample2.csv')
 
 cursor.execute("""
     SELECT stock_symbol, post_id, dt, title, body, flair, upvote_ratio 
@@ -37,4 +37,4 @@ cursor.execute("""
 """)
 sample3 = cursor.fetchall()
 random_sample3 = random.sample(sample3, 500)
-random_sample3 = pd.DataFrame.to_csv()
+random_sample3 = pd.DataFrame.to_csv('sample3.csv')
