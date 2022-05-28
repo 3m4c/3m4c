ca99f59dd83ac6dcee81d43aecafd0186545d46a
Emanuele Coccia <emanuele.coccia@studbocconi.it>
2022-05-29 00:51:39 +0200

    Update create_tables.sql

--- WSB/create_tables.sql
+++ WSB/create_tables.sql
@@ -6,22 +6,6 @@ CREATE TABLE stock (
     is_etf BOOLEAN NOT NULL
 );
 
-CREATE TABLE stock_price (
-    stock_id INTEGER NOT NULL,
-    stock_symbol TEXT NOT NULL,
-    dt TIMESTAMP WITHOUT TIME ZONE NOT NULL,
-    open NUMERIC NOT NULL,
-    high NUMERIC NOT NULL,
-    low NUMERIC NOT NULL,
-    close NUMERIC NOT NULL,
-    volume NUMERIC NOT NULL,
-    PRIMARY KEY (stock_id, dt),
-    CONSTRAINT fk_stock FOREIGN KEY (stock_id) REFERENCES stock (id)
-);
-
-CREATE INDEX ON stock_price (stock_id, dt DESC);
-SELECT create_hypertable('stock_price', 'dt');
-
 CREATE TABLE mention (
     stock_id INTEGER,
     stock_symbol TEXT NOT NULL,
